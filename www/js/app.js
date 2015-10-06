1ce20e4fad5ec41b5e8a2a9a033d99b28c61fd79
Victor Aguilar C <jvacx.log@gmail.com>
2015-10-06 08:08:47 -0400

    funcionamiento del formulario de registro listo

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,12 @@
 var $ = jQuery;
 
+var SERVER = "http://api.devfest.xyz";
+var SPEAKERS_ENDPOINT = SERVER + "/api/devfest/speakers/";
+var SPEECHES_ENDPOINT = SERVER + "/api/devfest/sponsors/";
+var SPONSORS_ENDPOINT = SERVER + "/api/devfest/speeches/";
+var REGISTER_ENDPOINT = SERVER + "/api/devfest/attends/";
+
+
 $(document).on("ready", function(){
 
 
@@ -7,6 +14,18 @@ $(document).on("ready", function(){
     var $navbar = $('.navbar');
 
     var valH = $(window).height();
+
+    // Preloader
+    var preloader = new $.materialPreloader({
+        position: 'top',
+        height: '5px',
+        col_1: '#159756',
+        col_2: '#da4733',
+        col_3: '#3b78e7',
+        col_4: '#fdba2c',
+        fadeIn: 200,
+        fadeOut: 200
+    });
 
     // Intro block.
     $intro.css({
@@ -17,33 +36,78 @@ $(document).on("ready", function(){
         {
             "id": 1,
             "full_name": "Victor Aguilar",
-            "positio": "CEO in Xiberty",
+            "position": "CEO in Xiberty",
             "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exe",
             "banner": null,
             "photo": "http://api.devfest.xyz/media/photos/2015/10/06/avatar.jpg",
             "info": "http://api.devfest.xyz/media/cvs/2015/10/06/Convenio-CINEMATECA.pdf",
-            "link_talk": "",
-            "twitter": "",
-            "github": "",
-            "google_plus": ""
+            "talk": "http://api.devfest.xyz/media/cvs/2015/10/06/Convenio-CINEMATECA.pdf",
+            "twitter": "https://twitter.com/jvacx",
+            "github": "https://github.com/jvacx",
+            "google_plus": "https://plus.google.com/"
         }
     ];
 
-
-    // Speaker cards.
-    var $menu = $("[data-card-menu]");
-    var $back = $("[data-card-back]");
-    $menu.click(function(){
-        $(".card, body").toggleClass("show-menu");
+    // Populate SPEAKERS
+    $.get( SPEAKERS_ENDPOINT, function( data ) {
+        Notary.populate("speaker", data, "speakers", function($dom, obj){
+            var $menu = $dom.find("[data-card-menu]");
+            var $back = $dom.find("[data-card-back]");
+            var $card = $dom.find(".speaker-card");
+            $menu.click(function(){
+                $card.toggleClass("show-menu");
+            });
+            $back.click(function(){
+                $card.toggleClass("show-menu");
+            });
+        });
     });
-    $back.click(function(){
-        $(".card, body").toggleClass("show-menu");
-    });
 
-    // Populate speakers
+    var $btnRegister = $("#btnRegister");
+    var $registerForm = $("#registerForm");
+
+    $btnRegister.click(function(e){
+        e.preventDefault();
+
+        preloader.on();
+
+        $registerForm.validate(function(success){
+
+            preloader.off();
+
+            if(success) {
+                var obj = Notary.makeAttende();
 
 
-    Notary.populate("speaker", speakers, "speakers");
+                console.log(obj);
+
+                $.ajax({
+                    url: REGISTER_ENDPOINT,
+                    method: 'POST',
+                    data: obj,
+                    success: function(data, textStatus, jqXHR){
+                        $registerForm.trigger("reset");
+                        Materialize.toast('Te has registrado exitosamente!', 4000);
+                    },
+                    error: function( jqXHR, textStatus, errorThrown){
+                        Materialize.toast('Hubo un error en el registro', 2000);
+                    },
+                    beforeSend: function( jqXHR, settings){
+                        preloader.on();
+                    },
+                    complete: function(jqXHR, textStatus){
+                        preloader.off();
+                    },
+                })
+            }
+        });
+
+    })
+
+
+
+
+
     // data-model
     // data-text
     // data-link
--- www/js/notary.js
+++ www/js/notary.js
@@ -3,27 +3,98 @@ var Notary = Notary || {};
 
     var NotaryBase = {
 
-        populate: function(template, array, destiny) {
+        populate: function(template, array, destiny, callback) {
             // template, array and destiny are  values from dom data-*
             var $template = $('[data-template="'+template+'"]');
             var $destiny = $('[data-container="'+destiny+'"]');
 
-            $.each([ 52, 97 ], function( index, value ) {
+            var self = this;
+            $.each(array, function( index, obj ) {
+                var $newObj = self.makeObject($template, obj);
+                $newObj.appendTo($destiny);
+
+                // execute item callback
+                if (!!callback)
+                    callback($newObj, obj);
 
             });
-
-            console.log($template);
-            console.log($destiny);
+            $template.detach();
         },
 
-        makeTexts: function(dom, obj){
+        makeObject: function($template, obj){
+            var $obj = $template.clone();
+            $obj.removeAttr('data-template');
+            this.makeTexts($obj, obj);
+            this.makeLinks($obj, obj);
+            this.makeImages($obj, obj);
 
+            return $obj;
         },
-        makeLinks: function(dom, obj){
 
+        makeTexts: function($dom, obj){
+            var $texts = $dom.find('[data-value]');
+            $.each($texts, function( index, item ) {
+                var val = $(item).attr('data-value');
+                $(item).text(obj[val]);
+            });
         },
-        makeImages: function(dom, obj){
 
+        makeLinks: function($dom, obj){
+            var $links = $dom.find('[data-link]');
+            $.each($links, function( index, item ) {
+                var val = $(item).attr('data-link');
+                $(item).attr("href", obj[val]);
+            });
+        },
+
+        makeImages: function($dom, obj){
+            var $images = $dom.find('[data-image]');
+            $.each($images, function( index, item ) {
+                var val = $(item).attr('data-image');
+                $(item).attr("src", obj[val]);
+            });
+        },
+
+        makeAttende: function(){
+            var $form = $("#registerForm");
+            var $values = $form.find("[data-register]");
+
+            var reqObj = {};
+            $.each($values, function( index, item ) {
+                var key = $(item).attr('data-register');
+                var value = $(item).val();
+
+                reqObj[key] =  value;
+            });
+
+
+            var $boxes = $form.find('[data-tech]');
+
+            var skills = "";
+
+            console.log($boxes);
+
+            $.each($boxes, function( index, item ) {
+                if($(item).is(':checked')){
+                    skills += $(item).attr("data-tech") + ",";
+                }
+            });
+
+            var other = $form.find("#otherTech").val().toUpperCase();
+
+            if(!!other){
+                if (other.length > 0)
+                    skills += other;
+                else
+                    skills = skills.substring(0, other.length - 1);
+            }
+
+            if(skills.length == 0)
+                skills += ".";
+
+            reqObj["skills"] = skills;
+
+            return reqObj;
         }
     }
 
